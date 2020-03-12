40f6950ec18d1babcdbcb1cbb4190a69e70b711f
koharup <anandpita18@icloud.com>
2020-03-13 04:12:36 +0900

    ImageActivityの追加

---
+++ app/src/main/java/com/sano/picchi/muse/ImageActivity.java
@@ -0,0 +1,14 @@
+package com.sano.picchi.muse;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
